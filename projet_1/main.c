584ef44edac90faaac7c9ddc36efdbe49eb5bada
MaloDaHood <malomr2002@gmail.com>
2021-01-28 23:01:05 +0100

    Non fonctionnel et pas fini

--- projet_1/main.c
+++ projet_1/main.c
@@ -1,28 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
-//#include "proto.h"
-
-void copie(int tableauOriginal[], int tableauCopie[], int tailleTableau);
+#include "proto.h"
 
 int main(int argc, char *argv[])
 {
-    int tableauOriginal[4]={10, 15, 3}, tableauCopie[4]={0}, i;
-    printf("=== Tableau Original ===\n");
-    for(i=0;i<4;i++)
-    {
-        printf("%d\n", tableauOriginal[i]);
-    }
-    printf("=== Tableau Copie ===\n");
-    copie(tableauOriginal, tableauCopie, 4);
+    int tableau[4]={15, 81, 22, 13};
+    ordonnerTableau(tableau, 4);
     return 0;
 }
 
-void copie(int tableauOriginal[], int tableauCopie[], int tailleTableau)
+void ordonnerTableau(int tableau[], int tailleTableau)
 {
-    int i;
+    int i, valeurTableau, x;
     for(i=0;i<tailleTableau;i++)
     {
-        tableauCopie[i]=tableauOriginal[i];
-        printf("%d\n", tableauCopie[i]);
+        valeurTableau=tableau[i];
+        stock(valeurTableau, i);
     }
+}
+
+int stock(int valeurTableau, int i)
+{
+    int valeurZero, valeurOne, valeurTwo, valeurThree, x=0-1; 
+    if(i==0)
+    {
+        valeurZero=valeurTableau;
+        //printf("%d\n%d", valeurTableau, valeurZero);  // OK
+    }
+    else if(i==1)
+    {
+        valeurOne=valeurTableau;
+        //printf("%d\n%d", valeurTableau, valeurOne);  // OK
+    }
+    else if(i==2)
+    {
+        valeurTwo=valeurTableau;
+        //printf("%d\n%d", valeurTableau, valeurTwo);  // OK 
+    }
+    else if(i==3)
+    {
+        valeurThree=valeurTableau;
+        //printf("%d\n%d", valeurTableau, valeurThree);  // OK
+    }
+    else
+    {
+        printf("ERREUR");
+        return 1;
+    }
+
+    if(valeurZero>x&&valeurOne>x&&valeurTwo>x&&valeurThree>x)  // ERREUR ICI
+    {
+        printf("%d\n%d\n%d\n%d", valeurZero, valeurOne, valeurTwo, valeurThree);  // NON OK
+        comparateur(valeurZero, valeurOne, valeurTwo, valeurThree);
+    }
+}
+
+int comparateur(int nb0, int nb1, int nb2, int nb3)
+{
+    //printf("%d\n %d\n %d\n %d\n", nb0, nb1, nb2, nb3);  // NON OK 
 }
---
+++ projet_1/proto.h
@@ -0,0 +1,3 @@
+void ordonnerTableau(int tableau[], int tailleTableau);
+int stock(int valeurTableau, int i);
+int comparateur(int nb0, int nb1, int nb2, int nb3);
