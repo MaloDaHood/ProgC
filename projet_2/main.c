71d3153998f14eeba974843a36c3d0365417bc80
MaloDaHood <malomr2002@gmail.com>
2021-02-04 21:57:30 +0100

    préprocesseur

--- projet_2/main.c
+++ projet_2/main.c
@@ -1,13 +1,10 @@
-#include <stdio.h>
-#include <string.h>
 #include "main.h"
 
 int main(int argc, char *argv[])
 {
-    char chaine[100], nom[100];
-    printf("Tu t'appel comment ?");
-    scanf("%s", nom);
-    sprintf(chaine, "Tu t'appel %s !", nom);
-    printf("%s", chaine);
+    int age;
+    printf("Quel age ?");
+    scanf("%d", &age);
+    printf("%d", age);
     return 0;
 }
---
+++ projet_2/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN
+#define MAIN
+
+#include <stdio.h>
+#include <string.h>
+
+#endif
