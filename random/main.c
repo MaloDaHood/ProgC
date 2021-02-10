9ad5dafff39a3b40b1b2c528b37b7f21eddf9534
MaloDaHood <malomr2002@gmail.com>
2021-02-10 11:23:12 +0100

    fini

--- random/main.c
+++ random/main.c
@@ -3,22 +3,43 @@
 
 void afficher(int nombreDeFois)
 {
-    int i;
+    int i=0;
     for(i=0;i<nombreDeFois;i++)
     {
-        printf("%d", i);
+        printf("T");
     }
+}
+
+void afficher2(int nombreDeFois)
+{
+    int i=0;
+    for(i=nombreDeFois;i>0;i--)
+    {
+        printf("T");
+    }   
 }
 
 int main(int argc, char *argv[])
 {
-    int nombreDeFois=0, i;
+    int repeat=0, nbFois=0;
     printf("Combien ?");
-    scanf("%d", &nombreDeFois);
-    for(i=nombreDeFois;i>0;i--)
+    scanf("%d", &repeat);
+    nbFois=repeat;
+    while(repeat!=0)
     {
-        afficher(nombreDeFois-i);
-        printf("\n");
+        repeat--;
+        int nombreDeFois=0, i=0;
+        nombreDeFois=nbFois;
+        for(i=nombreDeFois;i>0;i--)
+        {
+            afficher(nombreDeFois-i);
+            printf("\n");
+        }
+        for(i=0;i<nombreDeFois;i++)
+        {
+            afficher2(nombreDeFois-i);
+            printf("\n");
+        }
     }
     return 0;
 }
