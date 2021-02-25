fabfa603e8c5e7112ddde7dcbcdca945f7497f68
MaloDaHood <malomr2002@gmail.com>
2021-02-25 18:43:30 +0100

    fini

--- html_format/main.c
+++ html_format/main.c
@@ -3,26 +3,48 @@
 
 int main(int argc, char *argv[])
 {
-    FILE* fichier = NULL;
-    int caractereActuel = 0;
- 
-    fichier = fopen("html.txt", "r+");
- 
-    if (fichier != NULL)
+    FILE* fichierInput = NULL;
+    FILE* fichierOutput = NULL;
+    int caractere = 0;
+    char emplacementFichierInput[100], emplacementFichierOutput[100];
+    printf("Quel est le fichier a convertir ?\nVeuillez indiquer le chemin d'acces si le fichier ne se trouve pas dans le meme dossier que le programme.\n");
+    scanf("%s", emplacementFichierInput);
+    printf("\nVeuillez indiquer l'emplacement et le nom du fichier de sorti a creer (ainsi que son extension).\n");
+    scanf("%s", emplacementFichierOutput);
+    fichierInput = fopen(emplacementFichierInput, "r");
+    fichierOutput = fopen(emplacementFichierOutput, "w");
+    if (fichierInput != NULL && fichierOutput != NULL)
     {
-        // Boucle de lecture des caractères un à un
         do
         {
-            caractereActuel = fgetc(fichier); // On lit le caractère
-            if(caractereActuel == '<')
+            caractere = fgetc(fichierInput);
+            if (caractere == '<')
             {
-                printf("OUI");
+                fprintf(fichierOutput, "&lt;");
             }
-            printf("%c", caractereActuel); // On l'affiche
-        } while (caractereActuel != EOF); // On continue tant que fgetc n'a pas retourné EOF (fin de fichier)
- 
-        fclose(fichier);
+            else if (caractere == '>')
+            {
+                fprintf(fichierOutput, "&gt;");
+            }
+            else if (caractere == '"')
+            {
+                fprintf(fichierOutput, "&quot;");
+            }
+            else if (caractere == EOF)
+            {
+                break;
+            }
+            else
+            {
+                fputc(caractere, fichierOutput);
+            }
+        } while (caractere != EOF);
+        fclose(fichierInput);
+        fclose(fichierOutput);
     }
- 
+    else
+    {
+        printf("Fichier introuvable ou chemin d'acces introuvable.");
+    }
     return 0;
 }
